3dad3b8f8ebdf8fd4ec6e45a7913aa96d841e570
gooseandmegander <meganawilliamson@gmail.com>
2018-09-19 10:30:49 -0700

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,38 @@ import { connect } from 'react-redux';
 
 import logo from '../logo.svg';
 import '../styles/App.css';
+import { getChars } from '../actions';
 // pull in actions from action/index
 
 class App extends Component {
   componentDidMount() {
     // call our action
+    this.props.getChars();
   }
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         {this.props.fetching ? (
           <img src={logo} className="App-logo" alt="logo" />
         ) : (
-          <ul>
-            {this.props.chars.map(char => {
-              return <li key={char.name}>{char.name}</li>;
-            })}
-          </ul>
-        )}
+            <ul>
+              {this.props.chars.map(char => {
+                return <li key={char.name}>{char.name}</li>;
+              })}
+            </ul>
+          )}
       </div>
     );
   }
 }
 
 // our mapDispatchToProps needs to have two properties inherited from state
+const mapStateToProps = state => {
+  console.log('state', state)
+  return {
+    chars: state.charsReducer.chars,
+  }
+}
 // the chars and the fetching boolean
-export default connect(null, {
-  /* actions go here */
-})(App);
+export default connect(mapStateToProps, { getChars })(App);
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,20 @@
-import /* we need our action types here*/ '../actions';
+import { FETCHING_CHARS, FETCHED_CHARS, FETCHING_ERROR } from '../actions';
 const initialState = {
+  chars: [],
+  fetching: false,
+  fetched: false,
+  error: null,
   // define a few properties here.
   // Array chars, Boolean fetching, Boolean fetched, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING_CHARS:
+      return { ...state, fetching: true, error: null };
+    case FETCHED_CHARS:
+      return { ...state, fetching: false, chars: [...action.payload], error: null };
+    case FETCHING_ERROR:
+      return { ...state, fetching: false, error: action.payload };
     // Fill me in with the important reducers
     // action types should be FETCHING, FETCHED, and ERROR
     // your switch statement should handle all of these cases.
